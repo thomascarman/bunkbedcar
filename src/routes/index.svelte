df83ee8c1c52c6e3e7589bf7a3dc5e63ec601785
Thomas Carman <thomascarman@users.noreply.github.com>
2021-11-22 21:59:22 -0800

    Add Inital page for index.svelte About Project

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<main>
+  <h1>BunkBedCar.com</h1>
+  <p>
+    Welcome to our future website featuring our BunkBedCar (coined by our
+    daughter Penelope, age 6). She is a 1977 GMC Eleganza II based here in Las
+    Vegas, NV.
+  </p>
+
+  <img src="/SolarPrep.jpg" alt="Prep for Solar Install" />
+</main>
+
+<style lang="scss">
+  :global(body) {
+    padding: 0;
+    margin: 0;
+  }
+
+  :global(*) {
+    box-sizing: border-box;
+  }
+
+  main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: clamp(720px, 100vw, 980px);
+    margin: 0 auto;
+    padding: 0;
+  }
+
+  p {
+    max-width: 70ch;
+  }
+
+  img {
+    width: 100%;
+    max-width: 70ch;
+  }
+</style>
